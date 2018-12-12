bf8d42e9530d8e5694e12191bc1432f3280abf30
Patrick Valsecchi <patrick@thus.ch>
2018-12-12 15:31:35 +0100

    Fixed integration with SCM

--- api/mapfish_print_logs/services/source.py
+++ api/mapfish_print_logs/services/source.py
@@ -20,15 +20,15 @@ def get_source(request):
             PrintAccounting.app_id.like(utils.quote_like(app_id) + ":%")
         )
     ).order_by(PrintAccounting.completion_time.desc()).offset(pos).limit(JOB_LIMIT + 1).all()
-
-    scm_refresh_url = f'{SCM_URL_EXTERNAL}1/refresh/{source}/{key}' \
+    source_key = config['sources'][source]["key"]
+    scm_refresh_url = f'{SCM_URL_EXTERNAL}1/refresh/{source}/{source_key}' \
         if SCM_URL_EXTERNAL is not None else None
 
     return {
         'source': source,
         'jobs': [log for log in logs[:JOB_LIMIT]],
         'scm_refresh_url': scm_refresh_url,
-        'config': get_config_info(source, key),
+        'config': get_config_info(source, source_key),
         'next_pos': None if len(logs) <= JOB_LIMIT else pos + JOB_LIMIT,
         'prev_pos': None if pos == 0 else max(0, pos - JOB_LIMIT)
     }
