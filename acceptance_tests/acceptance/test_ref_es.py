34a14b2566530bb38d84898bf923e28b76b275f4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-12-16 15:53:26 +0100

    f

--- acceptance_tests/acceptance/test_ref_es.py
+++ acceptance_tests/acceptance/test_ref_es.py
@@ -1,20 +1,20 @@
 # we have three non-debug logs and a LOG_LIMIT of 2 => two pages
 
 
-def test_ok_page1(api_connection_es, print_job):
-    page = api_connection_es.get("logs/ref", params={"ref": print_job})
+def test_ok_page1(api_connection_es, print_job_es):
+    page = api_connection_es.get("logs/ref", params={"ref": print_job_es})
     print(page)
-    assert print_job in page
-    assert f"Starting job {print_job}" in page
+    assert print_job_es in page
+    assert f"Starting job {print_job_es}" in page
     assert "DEBUG" not in page
     assert page.count('<tr class="level-') == 3
 
 
-def test_ok_page2(api_connection_es, print_job):
-    page = api_connection_es.get("logs/ref", params={"ref": print_job, "pos": 2})
+def test_ok_page2(api_connection_es, print_job_es):
+    page = api_connection_es.get("logs/ref", params={"ref": print_job_es, "pos": 2})
     print(page)
-    assert print_job in page
-    assert f"Finished job {print_job}" in page
+    assert print_job_es in page
+    assert f"Finished job {print_job_es}" in page
     assert "DEBUG" not in page
     assert page.count('<tr class="level-') == 1
 
@@ -23,16 +23,16 @@ def test_unknown(api_connection_es):
     api_connection_es.get("logs/ref", params={"ref": "unknown"}, expected_status=404)
 
 
-def test_filter(api_connection_es, print_job):
+def test_filter(api_connection_es, print_job_es):
     page = api_connection_es.get(
         "logs/ref",
         params={
-            "ref": print_job,
+            "ref": print_job_es,
             "filter_loggers": "org.mapfish.print",
             "min_level": "10000",
         },
     )
     print(page)
-    assert print_job in page
-    assert f"Starting job {print_job}" not in page
+    assert print_job_es in page
+    assert f"Starting job {print_job_es}" not in page
     assert "Some &lt;b&gt;debug&lt;/b&gt;" in page
