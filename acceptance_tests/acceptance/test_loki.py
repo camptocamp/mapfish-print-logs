af9a5a9c2ccdff10a89c30971b68ec642a315515
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-12-16 16:35:58 +0100

    f

--- acceptance_tests/acceptance/test_loki.py
+++
@@ -1,7 +0,0 @@
-from mapfish_print_logs.loki import escape
-
-
-def test_escape():
-    assert escape("11'22") == "11'22"
-    assert escape('11"22') == r"11\"22"
-    assert escape(r"11\22") == r"11\\22"
