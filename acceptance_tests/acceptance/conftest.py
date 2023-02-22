effc8a0b280b693d511d4634c0bcd12ab1a9f3ad
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-02-22 09:02:50 +0100

    Bump print version to 3.28

--- acceptance_tests/acceptance/conftest.py
+++ acceptance_tests/acceptance/conftest.py
@@ -1,8 +1,10 @@
+import functools
+import json
 import logging
 
 import pytest
 from c2cwsgiutils.acceptance import utils
-from c2cwsgiutils.acceptance.connection import Connection
+from c2cwsgiutils.acceptance.connection import CacheExpected, Connection, check_response
 from c2cwsgiutils.acceptance.print import PrintConnection
 
 from .fake_print_logs import gen_fake_print_logs
@@ -59,3 +61,38 @@ class PrintLogConnection(PrintConnection):
         ref = report.url.split("/")[-1]
         gen_fake_print_logs(ref, es_url=ES_URL)
         return ref
+
+    def get_capabilities(self, app: str):
+        return self.get_json(app + "/capabilities.json", cors=False, cache_expected=CacheExpected.DONT_CARE)
+
+    def get_example_requests(self, app: str):
+        samples = self.get_json(
+            app + "/exampleRequest.json", cors=False, cache_expected=CacheExpected.DONT_CARE
+        )
+        out = {}
+        for name, value in samples.items():
+            out[name] = json.loads(value)
+        return out
+
+    def get_pdf(self, app: str, request, timeout: int = 60):
+        create_report = self.post_json(
+            app + "/report.pdf", json=request, cors=False, cache_expected=CacheExpected.DONT_CARE
+        )
+        LOG.debug("create_report=%s", create_report)
+        ref = create_report["ref"]
+
+        status = utils.retry_timeout(functools.partial(self._check_completion, ref), timeout=timeout)
+        LOG.debug("status=%s", repr(status))
+        assert status["status"] == "finished"
+
+        report = self.get_raw("report/" + ref, cors=False, cache_expected=CacheExpected.DONT_CARE)
+        assert report.headers["Content-Type"] == "application/pdf"
+        return report
+
+    def _check_completion(self, ref: str):
+        status = self.get_json(
+            "status/{ref}.json".format(ref=ref), cors=False, cache_expected=CacheExpected.DONT_CARE
+        )
+        if status["done"]:
+            return status
+        return None
